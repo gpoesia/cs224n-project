bcc9209f5bf6b6430b658f7a51df444fd34b05a2
leg2015 <leg2015@gmail.com>
2020-03-01 23:07:54 -0800

    changed frequency based encoder to keep first and last characters

--- baseline.py
+++ baseline.py
@@ -3,6 +3,7 @@ import random
 import math
 from nltk.util import ngrams
 from collections import Counter
+import pprint
 
 class UniformEncoder(AutoCompleteEncoder):
     'Encodes a string by removing characters uniformly at random with fixed probability.'
@@ -60,6 +61,41 @@ class FrequencyEncoder(AutoCompleteEncoder):
         return list(zip(*[text[i:] for i in range(n)]))
 
     def encode(self, s):
+        """takes a sequence and iteratively compresses the most  frequent n-gram to 
+        its first and last character until the sequence is compressed to compression_rate
+        
+        Arguments:
+            s {[string]} -- [sequence to compress]
+        """
+
+        # since strings are immutable, grab a copy
+        sent = s
+        # take floor b/c guarantees at least one char removed during compression
+        # if math.floor(self.compression_rate*len(s)) >= self.MIN_SIZE else self.MIN_SIZE
+        seq_len = math.floor(self.compression_rate*len(s))
+        # get all n-grams from sequence
+        seq_ngram = FrequencyEncoder.zipngram(s, self.n_gram)
+        # get frequency counts of all n-grams in sequence
+        ngram_counts = sorted(list(
+            {ngram: self.ngram_counter[ngram] for ngram in seq_ngram}.items()), key=lambda count: -count[1])
+        while len(sent) > seq_len:
+            # most frequent n-gram
+            freq = ''.join(ngram_counts.pop(0)[0])  #self.ngram_counter.most_common(1)[0][0])
+            # get first, last characters of most frequent n-gram
+            freq_comp = freq[0] + freq[-1]
+            # remove first instance of n-gram from sequence
+            sent = sent.split(freq, maxsplit=1)[
+                0] + freq_comp + sent.split(freq, maxsplit=1)[1]
+            # recalculate n-grams + frequencies in shortened sequence
+            seq_ngram = FrequencyEncoder.zipngram(sent, self.n_gram)
+            ngram_counts = list(
+                {ngram: self.ngram_counter[ngram] for ngram in seq_ngram}.items())
+            ngram_counts = sorted(
+                ngram_counts, key=lambda count: count[1], reverse=True)
+
+        return sent
+
+    def old_encode(self, s):
         """takes a sequence and iteratively removes most frequent n-grams until
         sequence is compressed to compression_rate
         
@@ -76,13 +112,15 @@ class FrequencyEncoder(AutoCompleteEncoder):
         # get all n-grams from sequence
         seq_ngram = FrequencyEncoder.zipngram(s, self.n_gram)
         # get frequency counts of all n-grams in sequence
-        ngram_counts = sorted(list({ngram : self.ngram_counter[ngram] for ngram in seq_ngram}.items()), key= lambda count : count[1])
+        
+        ngram_counts = sorted(list({ngram : self.ngram_counter[ngram] for ngram in seq_ngram}.items()), key= lambda count : -count[1])
         while len(sent) > seq_len:
             # most frequent n-gram
             freq = ''.join(ngram_counts.pop(0)[0])
             # remove first instance of n-gram from sequence
             sent = sent.split(freq, maxsplit=1)[0] + sent.split(freq, maxsplit=1)[1]
             # recalculate n-grams + frequencies in shortened sequence
+            print(f"after: {sent}")
             seq_ngram = FrequencyEncoder.zipngram(sent, self.n_gram)
             ngram_counts = list(
                 {ngram: self.ngram_counter[ngram] for ngram in seq_ngram}.items())
@@ -102,6 +140,17 @@ class FrequencyEncoder(AutoCompleteEncoder):
             [list[string]] -- [list of compressed sequences as strings]
         """
         return [self.encode(s) for s in b]
+
+    def old_encode_batch(self, b):
+        """compress a batch of sequences
+        
+        Arguments:
+            b {[list[string]]} -- [list of strings containing sequences to compress]
+        
+        Returns:
+            [list[string]] -- [list of compressed sequences as strings]
+        """
+        return [self.old_encode(s) for s in b]
 
     def is_optimizeable(self):
         return False
